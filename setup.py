c9df1bdb7b063485234ba2a3c603dd7de2fa0232
Ajay <ajayrao4376@gmail.com>
2026-01-12 13:08:06 +0530

    Completed the AI Assistant

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import find_packages, setup
+
+
+setup(name="GenAi ai project", version="0.0.1",
+      author="Ajay Rao" ,author_email="ajayrao4376@gmail.com",
+      packages=find_packages())
